a0c38c7cb2cbf037b3eb52ae7e471f2afd9b7e7c
Ishak Alpfidan <isaac.alpines@gmail.com>
2021-09-01 09:29:33 +0300

    Add project files.

---
+++ WebGridControl/Controllers/HomeController.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using WebGridControl.Models;
+
+namespace WebGridControl.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            Kitap.GenerateFakeData(50);
+
+            return View(Kitap.Kitaplar);
+        }
+
+        [HttpPost]
+        public ActionResult Index(string search)
+        {
+            List<Kitap> filtrelenmis =
+                Kitap.Kitaplar.Where(x =>
+                    x.Adi.ToLower().Contains(search) ||
+                    x.Yazar.ToLower().Contains(search) ||
+                    x.YayinTarihi.ToString().Contains(search) ||
+                    x.Fiyat.ToString().Contains(search)).ToList();
+
+            return View(filtrelenmis);
+        }
+    }
+}
---
+++ WebGridControl/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="WebGridControl.MvcApplication" Language="C#" %>
